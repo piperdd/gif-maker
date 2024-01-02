c1e48553bcef01de9129719c84aeb3e822147707
piperdd <harunazh2003@gmail.com>
2024-01-02 21:27:28 +0000

    added selectable crop box

--- script.js
+++ script.js
@@ -1,30 +1,32 @@
-const image = document.getElementById('image');
+// const image = document.getElementById('image');
 
-var cropper = new Cropper(image, {
-    aspectRatio: 1,
-    // viewMode: 1,
-    zoomable: false,
-});
+// var cropper = new Cropper(image, {
+//     aspectRatio: 1,
+//     // viewMode: 1,
+//     zoomable: false,
+// });
 
-const element = document.getElementById('photos');
+// const element = document.getElementById('photos');
 // const collection = element.querySelectorAll('.gam');
 
-document.getElementById("cropImageBtn").addEventListener('click', function(){
-    var croppedImage = cropper.getCroppedCanvas().toDataURL("image/png");
-    // element.children[1].style.display = "none";
-    document.getElementById('output').src = croppedImage;
-    document.getElementById('output').style.display = 'block';
-});
+// document.getElementById("cropImageBtn").addEventListener('click', function(){
+//     var croppedImage = cropper.getCroppedCanvas().toDataURL("image/png");
+//     // element.children[1].style.display = "none";
+//     document.getElementById('output').src = croppedImage;
+//     document.getElementById('output').style.display = 'block';
+// });
 
-document.getElementById("toggleBtn").addEventListener('click', function(){
-    var output = document.getElementById('output').style.display
-    if (output == 'none'){
-        document.getElementById('output').style.display = 'block';
-    }else{
-        document.getElementById('output').style.display = 'none';
-    }
-});
+// document.getElementById("toggleBtn").addEventListener('click', function(){
+//     var output = document.getElementById('output').style.display
+//     if (output == 'none'){
+//         document.getElementById('output').style.display = 'block';
+//     }else{
+//         document.getElementById('output').style.display = 'none';
+//     }
+// });
+// --------------------------------- //
 
+// --------------------------------- //
 Sortable.create(sortable, {
     animation: 150,
     // sort: true
@@ -36,8 +38,12 @@ var i = 0;
 
 var inputFile = document.getElementById("input-file");
 document.getElementById("submit").addEventListener('click', () => {
-    document.querySelector(".image-cropper").style.opacity = 1;
+    // document.querySelector(".image-cropper").style.opacity = 1;
     // dynamicaly add images to image list
+    // var croppers = [];
+    var cropper;
+    var srcImages = []
+    var focusImage = document.querySelector(".image-focus")
     for(i; i < inputFile.files.length; i++){
         console.log(inputFile.files[i]);
 
@@ -51,6 +57,16 @@ document.getElementById("submit").addEventListener('click', () => {
         img.src = URL.createObjectURL(inputFile.files[i])
         img.tabIndex = i;
 
+        // if(i == 0){
+        //     document.activeElement.blur()
+        //     img.focus();
+            
+        //     console.log(document.activeElement);
+        // }
+
+        var imgDiv = document.createElement('div');
+        imgDiv.classList.add("imgDiv");
+
         var tag = document.createElement("div");
         tag.classList.add("tag");
 
@@ -61,8 +77,10 @@ document.getElementById("submit").addEventListener('click', () => {
         deleteButton.textContent = "X";
         deleteButton.classList.add("delete-button");
 
+        imgDiv.appendChild(img);
+
         tag.appendChild(p);
-        imageOverlay.appendChild(img);
+        imageOverlay.appendChild(imgDiv);
         imageOverlay.appendChild(tag);
         imageOverlay.appendChild(deleteButton);
 
@@ -78,7 +96,41 @@ document.getElementById("submit").addEventListener('click', () => {
         nthImg.appendChild(copyButton);
         nthImg.appendChild(disableButton);
         imageList.appendChild(nthImg);
+
+        srcImages.push(img.src);
+
+        img.addEventListener("focus", (e) => {
+            console.log(e.target.tabIndex);
+            // cropper.destroy();
+            cropper.replace(srcImages[e.target.tabIndex])
+            // focusImage.src = srcImages[e.target.tabIndex];
+            // cropper = new Cropper(focusImage, {
+            //     aspectRatio: 1,
+            //     movable: false,
+            //     scalable: false,
+            //     zoomable: false,
+            // })
+        })
+        // var cropperIndividual = new Cropper(img, {
+        //     aspectRatio: 1,
+        //     // viewMode: 1,
+        //     // guides:false,
+        //     zoomable: false,
+        // });
+        // // cropperIndividual.destroy();
+        // croppers.push(cropperIndividual);
+
     }
+
+    
+    focusImage.src = srcImages[0];
+    cropper = new Cropper(focusImage, {
+        aspectRatio: 1,
+        movable: false,
+        scalable: false,
+        zoomable: false,
+    })
+    
 })
 
 imageList.addEventListener("click", (e) => {
@@ -90,3 +142,4 @@ imageList.addEventListener("click", (e) => {
         imageList.appendChild(clone);
     }
 })
+
